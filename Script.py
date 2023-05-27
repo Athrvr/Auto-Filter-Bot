21d08db1e52c430be575e3277c6de74b681a6ac0
Proffeser <133955134+Athrvr@users.noreply.github.com>
2023-05-27 13:53:15 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,67 @@
+class script(object):
+
+    START_TXT = """👋 Hello {},
+
+I can give any Movie and TV Series to auto filter method. 🥳
+
+All you have to do is add me to a group and give me admin. 😌
+
+I will take care of the rest. 😎"""
+
+    MY_ABOUT_TXT = """★ Server: <a href=https://www.heroku.com>Heroku</a>
+
+★ Database: <a href=https://www.mongodb.com>MongoDB</a>
+
+★ Language: <a href=https://www.python.org>Python</a>
+
+★ Library: <a href=https://pyrogram.org>Pyrogram</a>"""
+
+    MY_OWNER_TXT = """★ Name: PROFESSOR 
+
+★ Username: @jithurg
+
+★ ID: <code>5493832202</code>
+
+★ Country: INDIA 🇮🇳"""
+
+    STATUS_TXT = """🗂 Total Files: <code>{}</code>
+
+👤 Total Users: <code>{}</code>
+
+👥 Total Chats: <code>{}</code>
+
+✨ Used Storage: <code>{}</code>
+
+⚡️ Free Storage: <code>{}</code>"""
+
+    NEW_GROUP_TXT = """#NewGroup
+
+★ Title: {}
+
+★ ID: <code>{}</code>
+
+★ Total Members: {}
+
+★ Added by: {}"""
+
+    NEW_USER_TXT = """#NewUser
+
+★ Name: {}
+
+★ ID: <code>{}</code>"""
+
+    NO_RESULT_TXT = """#NoResult
+
+★ Group Name: {}
+
+★ Group ID: <code>{}</code>
+
+★ Name: {}
+
+★ Message: {}"""
+
+    REQUEST_TXT = """★ Name: {}
+
+★ ID: <code>{}</code>
+
+★ Message: {}"""
